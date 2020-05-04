1bc79b6616da886ad1c9ead3b9071219c1a0fd64
Jonathan Helmus <jjhelmus@gmail.com>
2020-05-04 16:38:56 -0500

    test-hotfix allows multiple subdirs

--- test-hotfix.py
+++ test-hotfix.py
@@ -37,28 +37,34 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('channel', help='channel name or url to download repodata from')
-    parser.add_argument('--subdir', help='subdir to download/diff', default=subdir)
+    parser.add_argument('--subdirs', nargs='*', help='subdir(s) to download/diff', default=(subdir, ))
     parser.add_argument('--diff-format', help='format to save diff as',
                         choices=('unified', 'context', 'html'), default='html')
     parser.add_argument('--context-numlines', help='context lines to show around diff',
                         type=int, default=5)
+    parser.add_argument('--use-cache', action='store_true', help='use cached repodata')
     args = parser.parse_args()
 
-    if not os.path.isdir(os.path.join(args.channel, args.subdir)):
-        os.makedirs(os.path.join(args.channel, args.subdir))
+    for subdir in args.subdirs:
+        if not os.path.isdir(os.path.join(args.channel, subdir)):
+            os.makedirs(os.path.join(args.channel, subdir))
     if '/' not in args.channel:
         channel_base_url = channel_map[args.channel]
-    clone_subdir(channel_base_url, args.subdir)
+    if not args.use_cache:
+        for subdir in args.subdirs:
+            clone_subdir(channel_base_url, subdir)
     subprocess.check_call(['python', args.channel + '.py'])
-    raw_repodata_file = os.path.join(args.channel, args.subdir, 'repodata_from_packages.json')
-    ref_repodata_file = os.path.join(args.channel, args.subdir, 'reference_repodata.json')
-    with open(raw_repodata_file) as f:
-        repodata = json.load(f)
-    out_instructions = os.path.join(args.channel, args.subdir, 'patch_instructions.json')
-    with open(out_instructions) as f:
-        instructions = json.load(f)
-    patched_repodata = _apply_instructions(args.subdir, repodata, instructions)
-    patched_repodata_file = os.path.join(args.channel, args.subdir, 'repodata-patched.json')
-    with open(patched_repodata_file, 'w') as f:
-        json.dump(patched_repodata, f, indent=2, sort_keys=True, separators=(',', ': '))
-    subprocess.call(['colordiff', ref_repodata_file, patched_repodata_file])
+    for subdir in args.subdirs:
+        print(subdir, "\n---------------")
+        raw_repodata_file = os.path.join(args.channel, subdir, 'repodata_from_packages.json')
+        ref_repodata_file = os.path.join(args.channel, subdir, 'reference_repodata.json')
+        with open(raw_repodata_file) as f:
+            repodata = json.load(f)
+        out_instructions = os.path.join(args.channel, subdir, 'patch_instructions.json')
+        with open(out_instructions) as f:
+            instructions = json.load(f)
+        patched_repodata = _apply_instructions(subdir, repodata, instructions)
+        patched_repodata_file = os.path.join(args.channel, subdir, 'repodata-patched.json')
+        with open(patched_repodata_file, 'w') as f:
+            json.dump(patched_repodata, f, indent=2, sort_keys=True, separators=(',', ': '))
+        subprocess.call(['colordiff', ref_repodata_file, patched_repodata_file])
