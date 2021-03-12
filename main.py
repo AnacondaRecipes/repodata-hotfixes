7b13fe18203a831485867a93ec24ecb812e97c44
Kai Tietz <ktietz@anaconda.com>
2021-03-12 11:42:31 +0100

    add version pinning of gast to 0.3.3 for tensorflow 2.4.1 version

--- main.py
+++ main.py
@@ -630,7 +630,7 @@ def patch_record_in_place(fn, record, subdir):
 
     if name.startswith('tensorflow-base') and version == '2.4.1':
         replace_dep(depends, 'gast', 'gast == 0.3.3')
-        depends.append('gast == 0.3.3')
+        depends.append('gast 0.3.3')
 
     ##############
     # constrains #
