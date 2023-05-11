6fe7e9751b8b95c93f2915b696d7a3bf786f7c8d
Ryan Keith <rkeith@anaconda.com>
2023-05-10 17:36:51 -0700

    Reverting changes.

--- main.py
+++ main.py
@@ -893,11 +893,6 @@ def patch_record_in_place(fn, record, subdir):
     # openssl >=1.1.1,<1.1.2.0a0 -> >=1.1.1a,<1.1.2a
     replace_dep(depends, "openssl >=1.1.1,<1.1.2.0a0", "openssl >=1.1.1a,<1.1.2a")
 
-    # openssl3 preventive measures
-    replace_dep(depends, "openssl !=1.1.1e", "openssl !=1.1.1e,<1.1.2a")
-    if name != "_anaconda_depends":
-        replace_dep(depends, "openssl", "openssl<1.1.2a")
-
     # kealib 1.4.8 changed sonames, add new upper bound to existing packages
     replace_dep(depends, "kealib >=1.4.7,<1.5.0a0", "kealib >=1.4.7,<1.4.8.0a0")
 
