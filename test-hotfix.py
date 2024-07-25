571832bcd7b9c4fda443bee48c3c4be031f820ab
Jamie Robertson <jamierobertsongames@gmail.com>
2024-07-25 10:46:31 +0100

    revert test-hotfix

--- test-hotfix.py
+++ test-hotfix.py
@@ -19,7 +19,6 @@ channel_map = {
     "main": "https://repo.anaconda.com/pkgs/main",
     "r": "https://repo.anaconda.com/pkgs/r",
     "msys2": "https://repo.anaconda.com/pkgs/msys2",
-    "numpy2": "https://anaconda.org/ad-testing/numpy",
 }
 
 
