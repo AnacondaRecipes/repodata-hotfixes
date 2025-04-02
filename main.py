2ec58bfe3cfd6408422a041027d17a00c1a073dd
skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
2025-04-02 15:32:09 +0300

    Set libtiff's upper bound <4.7.0 (#252)
    * Set libtiff's upper bound to <4.7.0
    
    * Remove obsolete platforms from README
    
    Co-authored-by: Charles Bousseau <16641587+cbouss@users.noreply.github.com>
    
    * Apply fix on all platforms for maintainability
    
    ---------
    
    Co-authored-by: Charles Bousseau <16641587+cbouss@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1112,6 +1112,19 @@ def patch_record_in_place(fn, record, subdir):
     if subdir.startswith("win-"):
         replace_dep(depends, "zeromq >=4.3.1,<4.4.0a0", "zeromq >=4.3.1,<4.3.2.0a0")
 
+    # Up to libtiff 4.4.0, the LIBTIFF_CURRENT version was 13, and LIBTIFF_AGE was 8.
+    # As of 4.5.0, LIBTIFF_CURRENT was decremented to 6, and LIBTIFF_AGE was reset.
+    # This change has major implications for Mach-O dynamic libraries on MacOS.
+    # We applied set_configure_script_version.patch in v4.5.0 to fix the issue.
+    # In v4.7.0 we removed that patch, so we have to constrain libtiff at runtime to <4.7.0.
+    replace_dep(depends, "libtiff >=4.0.8,<5.0a0", "libtiff >=4.0.8,<4.7.0")
+    replace_dep(depends, "libtiff >=4.0.9,<5.0a0", "libtiff >=4.0.9,<4.7.0")
+    replace_dep(depends, "libtiff >=4.0.10,<5.0a0", "libtiff >=4.0.10,<4.7.0")
+    replace_dep(depends, "libtiff >=4.1.0,<5.0a0", "libtiff >=4.1.0,<4.7.0")
+    replace_dep(depends, "libtiff >=4.2.0,<5.0a0", "libtiff >=4.2.0,<4.7.0")
+    replace_dep(depends, "libtiff >=4.5.0,<5.0a0", "libtiff >=4.5.0,<4.7.0")
+    replace_dep(depends, "libtiff >=4.5.1,<5.0a0", "libtiff >=4.5.1,<4.7.0")
+
     ##########################
     # single package depends #
     ##########################
