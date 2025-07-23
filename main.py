b5485fc421e2fee0171851d092b43ec72c852568
Andrii-Osipov <aosipov@anaconda.com>
2025-07-23 01:34:25 -0700

    fix linter

--- main.py
+++ main.py
@@ -738,11 +738,11 @@ def patch_record_in_place(fn, record, subdir):
     ######################
     # numpy dependencies #
     ######################
+
     # https://github.com/lmcinnes/pynndescent/blob/0.5.10/requirements.txt#L2
     # AttributeError: `np.infty` was removed in the NumPy 2.0 release. Use `np.inf` instead.
     if name == "pynndescent" and version in ["0.5.4", "0.5.10"]:
         replace_dep(depends, "numpy >=1.17", "numpy >=1.17,<2")
-    
 
     ###########
     # pytorch #
