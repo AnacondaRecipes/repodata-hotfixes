f8352a0216fa5e4d19a71211f0abf99f21700125
Eric Lundby <Eric.Lundby@gmail.com>
2026-06-15 14:59:46 -0600

    Yank lz4-c build 2

--- main.py
+++ main.py
@@ -185,6 +185,8 @@ REVOKED = {
         "langgraph-prebuilt-1.0.7-*_0.*",
         "langgraph-prebuilt-1.0.7-*_1.*",
         "langgraph-prebuilt-1.1.0-*_0.*",
+        # lz4-c 1.9.4 build 2 is broken; use build 1 instead.
+        "lz4-c-1.9.4-*_2.*",
     ],
 }
 
